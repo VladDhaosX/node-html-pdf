7135bd1ee739bb49e1910278af712904fb1857ee
abernal <deanva08@gmail.com>
2022-10-21 09:43:59 -0500

    Se agrega modulo de node-html-pdf para creacion de pdfs,se descarta por libreria desactualizada con boostrap.

--- api.js
+++ api.js
@@ -1,17 +1,19 @@
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    cors = require('cors');
 
-const dbocategoria = require('./dbcategoria');
-const morgan = require('morgan');
-const helmet = require('helmet');
+const
+    dbocategoria = require('./dbcategoria'),
+    morgan = require('morgan')
+helmet = require('helmet')
+path = require('path')
+swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json'),
+    fs = require('fs');
 
-const swaggerUi = require('swagger-ui-express'),
-    swaggerDocument = require('./swagger.json');
-
-var app = express();
-var router = express.Router();
+var app = express(),
+    router = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -22,12 +24,47 @@ app.use(
     swaggerUi.serve,
     swaggerUi.setup(swaggerDocument)
 );
-app.use(helmet());
-app.use(morgan('tiny'));
+
+// app.use(helmet());
+// app.use(morgan('tiny'));
 
 router.route('/categoria').get((request, response) => {
     dbocategoria.getCategorias().then(result => {
         response.json(result[0]);
+    })
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.use(express.static(path.join(__dirname, "./node_modules/bootstrap/dist/")));
+var pdf = require('html-pdf');
+app.get('/pdf', (req, res) => {
+    var html = fs.readFileSync('./index.html', 'utf8');
+    var config = {
+        "base": 'http://localhost:8090/',
+        "localUrlAccess": true,
+        "format": "A4"
+    }
+    pdf.create(html, config).toStream((err, pdfStream) => {
+        if (err) {
+            // handle error and return a error response code
+            console.log(err)
+            return res.sendStatus(500)
+        } else {
+            // send a status code of 200 OK
+            res.statusCode = 200
+
+            // once we are done reading end the response
+            pdfStream.on('end', () => {
+                // done reading
+                return res.end()
+            })
+
+            // pipe the contents of the PDF directly to the response
+            pdfStream.pipe(res)
+        }
     })
 })
 
